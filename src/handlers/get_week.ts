f47d2dd84aba9da5336a8973cfb980bed700adce
Popov Andrey <a.a.popov@student.khai.edu>
2022-09-24 19:51:25 +0300

    Create command and swap

--- src/handlers/get_week.ts
+++ src/handlers/get_week.ts
@@ -1,13 +1,74 @@
-import { Composer } from "telegraf";
-import day from "../models/day";
+import exp from "constants";
+import { Composer, Markup } from "telegraf";
+import Day from "../models/day";
 
-export default Composer.command("get_week", async (ctx) => {
-    const days = await day.find({})
 
-    let text = "Days:\n";
-    for (const day of days) {
-        text += `${day.num} ${day.name}\n`
+import service from "../service";
+
+const composer = new Composer();
+const get_keyboard_lessions = async (day_num: number) => {
+    const day = await Day.findOne({ num: day_num }).exec();
+    const buttons = [[]];
+    for (const [i, [a, b]] of day.lessions.entries()) {
+        if (a.empty && b.empty) continue;
+        const is_duo = !a.empty && !b.empty;
+        if (!a.empty) {
+            const txt = `${i + 1} ${is_duo ? ': Ч' : ''}`;
+            const callback_data = `lession:${day_num}@${i}@0`;
+            buttons[0].push(Markup.button.callback(txt, callback_data));
+        }
+        if (!b.empty) {
+            const txt = `${i + 1} ${is_duo ? ': З' : ''}`;
+            const callback_data = `lession:${day_num}@${i}@1`;
+            buttons[0].push(Markup.button.callback(txt, callback_data));
+        }
     }
+    buttons.push([Markup.button.callback('Назад', `back_w:${day_num}`)]);
 
-    ctx.reply(text);
+    if (service.EDIT_MODE.mode)
+        buttons.push([Markup.button.callback('Редактировать', `edit_mode:${day_num}`)]);
+
+    return Markup.inlineKeyboard(buttons);
+};
+
+const get_keyboard_days = async (selected_day: number) => {
+    const days = await Day.find({}).exec();
+    const buttons = [];
+    buttons.push([Markup.button.callback('Подробнее', `back_l:${selected_day}`)]);
+
+    for (const day of days) {
+        let text = day.name;
+
+        if (day.num === selected_day)
+            text = `✅ ${text}`;
+
+        if (day.num === service.get_current_day() && service.is_today_study())
+            text = `${text} 🔥`;
+
+        buttons.push([Markup.button.callback(text, `day:${day.num}`)]);
+    }
+    return Markup.inlineKeyboard(buttons);
+}
+
+composer.command("get_week", async (ctx) => {
+    const day = service.get_current_day();
+    const keyboard = await get_keyboard_days(day);
+    const text = await service.format_rosp(day);
+    await ctx.replyWithHTML(text, keyboard);
 });
+
+composer.action(/day:(\d+)/, async (ctx) => {
+    const day = parseInt(ctx.match[1]);
+    const keyboard = await get_keyboard_days(day);
+    const text = await service.format_rosp(day);
+
+    await ctx.editMessageText(text, {
+        parse_mode: 'HTML',
+        reply_markup: keyboard.reply_markup
+    });
+
+});
+
+
+
+export default composer;
--- src/service.ts
+++ src/service.ts
@@ -1,6 +1,18 @@
 import Days from "./models/day";
+import moment from "moment-timezone";
 
-const init_days = async() => {
+const TIME_ZONE = "Europe/Kiev";
+
+const TIMES_DEF = {
+    1: { start: "8:00", end: "9:35" },
+    2: { start: "9:50", end: "11:25" },
+    3: { start: "11:55", end: "13:30" },
+    4: { start: "13:45", end: "15:20" }
+}
+
+const EDIT_MODE = { mode: true };
+
+const init_days = async () => {
     if (await Days.count() !== 5) {
         await Days.create({ num: 0, name: "Понедельник" });
         await Days.create({ num: 1, name: "Вторник" });
@@ -8,7 +20,80 @@ const init_days = async() => {
         await Days.create({ num: 3, name: "Четверг" });
         await Days.create({ num: 4, name: "Пятница" });
     }
-}   
+}
 
 
-export default {init_days};
+const get_current_day = () => {
+    const day = moment().tz(TIME_ZONE).day() - 1;
+    if (day > 4) return 0;
+    return day;
+};
+const get_current_time = () => moment().tz(TIME_ZONE).format("HH:mm");
+
+const get_current_lesson = () => {
+    const current_time = get_current_time();
+    for (const key in TIMES_DEF) {
+        if (current_time >= TIMES_DEF[key].start && current_time <= TIMES_DEF[key].end) {
+            return parseInt(key);
+        }
+    }
+    return null;
+}
+
+const get_nomitaror_denomitaror = () => {
+    const now = moment().tz(TIME_ZONE);
+    const ans = now.isoWeek() % 2 !== 0;
+    if (moment().tz(TIME_ZONE).isoWeekday() > 5) return !ans;
+    return ans;
+}
+const is_today_study = () => {
+    const day = moment().tz(TIME_ZONE).isoWeekday()
+    return day !== 6 && day !== 7;
+}
+
+
+
+const format_rosp = async (day_num: number) => {
+    const day = await Days.findOne({ num: day_num }).exec();
+    const nomin = get_nomitaror_denomitaror();
+    const ntext = nomin ? "Чис" : "Знам";
+    let text = `Расписание на <b>${day.name} (${ntext})</b>:\n\n`;
+
+    const lession_num = get_current_lesson();
+    const day_now = get_current_day();
+
+    for (const [i, [l1, l2]] of day.lessions.entries()) {
+        const num = `<b>${i + 1}.</b> `;
+
+        const is_now = (lession_num === i + 1 && day_now === day_num && !is_today_study()) ? "🔥" : "";
+
+        text += `${num} ${TIMES_DEF[i + 1].start} - ${TIMES_DEF[i + 1].end}:`;
+
+        if (l1.empty && l2.empty)
+            text += `  <b>Нет пар</b>\n`;
+        else if (l1.empty || l2.empty)
+            text += `\n>    ${(l1.empty ? l2 : l1).title} ${is_now}\n`;
+        else
+            if (nomin)
+                text += `\n> Ч: ${l1.title}\n> З: <u><b>${l2.title}</b></u> ${is_now}\n`;
+            else
+                text += `\n> Ч: <u><b>${l1.title}</b></u> ${is_now}\n> З: ${l2.title}\n`;
+
+
+        text += "\n";
+    }
+
+    return text;
+}
+
+
+
+
+
+
+
+
+
+export default { init_days, get_nomitaror_denomitaror, TIMES_DEF, get_current_day, get_current_time, get_current_lesson, format_rosp, EDIT_MODE, is_today_study };
+
+
