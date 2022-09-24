af8f86d10082ebfb20276e652ae1ddfa5b836f18
Popov Andrey <a.a.popov@student.khai.edu>
2022-09-25 01:15:16 +0300

    Edit mode add

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,4 +1,6 @@
 import get_week from "./get_week"
 import get_rosp from "./get_rosp"
+import edit_mode from "./edit_mode"
 
-export default [get_week, get_rosp]
+
+export default [get_week, get_rosp, edit_mode]
