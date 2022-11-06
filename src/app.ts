460d35d52066b82c350673a983f3f2011abf69cc
Popov Andrey <a.a.popov@student.khai.edu>
2022-11-06 20:10:01 +0200

    Revert "fix try"
    This reverts commit 4bc23a09e3be9b81de08c678f99b6ca22de47749.

--- src/app.ts
+++ src/app.ts
@@ -10,33 +10,14 @@ const app = express()
 
 app.use(express.json())
 const bot = new Telegraf(BOT_TOKEN)
-const allowIds = [
-  727475349,
-  983486538
-]
+
 async function main() {
   console.log('Connecting to database...')
   await db.connect(MONGO_STRING, { dbName: "KHAI_BOT_TS" });
   await Service.init();
-  bot.use((ctx, next) => {
-    //log every message
-    console.log(ctx.message)
-    if (ctx.chat.type === 'group' || ctx.chat.type === 'supergroup') {
-      ctx.reply('Я не работаю в группах')
-      return
-    }
-    if (allowIds.includes(ctx.from.id)) {
-      return next();
-    }
-    else {
-      ctx.reply('Ты не имеешь доступа к этому боту')
-    }
-  })
+
   for (const handler of handlers)
     bot.use(handler);
-
-
-
 
   if (USE_WEBHOOK) {
     app.use(await bot.createWebhook({
